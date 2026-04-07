c4a894a4fc13426ddd193d606ae59677e83e4b54
magit notif <231088448+godsownf@users.noreply.github.com>
2026-04-07 09:19:59 +0100

    Update type.d.ts

--- type.d.ts
+++ type.d.ts
@@ -1,8 +1,6 @@
-
-
 interface IBrowser {
     /**
-     * Possible values :
+     * Possible values:
      * Amaya, Android Browser, Arora, Avant, Baidu, Blazer, Bolt, Camino, Chimera, Chrome,
      * Chromium, Comodo Dragon, Conkeror, Dillo, Dolphin, Doris, Edge, Epiphany, Fennec,
      * Firebird, Firefox, Flock, GoBrowser, iCab, ICE Browser, IceApe, IceCat, IceDragon,
@@ -11,79 +9,79 @@ interface IBrowser {
      * Mosaic, Mozilla, Netfront, Netscape, NetSurf, Nokia, OmniWeb, Opera [Mini/Mobi/Tablet],
      * Phoenix, Polaris, QQBrowser, RockMelt, Silk, Skyfire, SeaMonkey, SlimBrowser, Swiftfox,
      * Tizen, UCBrowser, Vivaldi, w3m, Yandex
-     */
+     /
     name: string | undefined;
 
     /**
      * Determined dynamically
-     */
+     /
     version: string | undefined;
 
     /**
      * Determined dynamically
      * @deprecated
-     */
+     /
     major: string | undefined;
 }
 
 interface IDevice {
     /**
      * Determined dynamically
-     */
+     /
     model: string | undefined;
 
     /**
-     * Possible type:
+     Possible type:
      * console, mobile, tablet, smarttv, wearable, embedded
-     */
+     /
     type: string | undefined;
 
     /**
-     * Possible vendor:
-     * Acer, Alcatel, Amazon, Apple, Archos, Asus, BenQ, BlackBerry, Dell, GeeksPhone,
+     Possible vendor:
+     *Acer, Alcatel, Amazon, Apple, Archos, Asus, BenQ, BlackBerry, Dell, GeeksPhone,
      * Google, HP, HTC, Huawei, Jolla, Lenovo, LG, Meizu, Microsoft, Motorola, Nexian,
      * Nintendo, Nokia, Nvidia, Ouya, Palm, Panasonic, Polytron, RIM, Samsung, Sharp,
      * Siemens, Sony-Ericsson, Sprint, Xbox, ZTE
-     */
+     /
     vendor: string | undefined;
 }
 
 interface IEngine {
     /**
-     * Possible name:
+     Possible name:
      * Amaya, EdgeHTML, Gecko, iCab, KHTML, Links, Lynx, NetFront, NetSurf, Presto,
      * Tasman, Trident, w3m, WebKit
-     */
+     /
     name: string | undefined;
     /**
      * Determined dynamically
-     */
+     /
     version: string | undefined;
 }
 
 interface IOS {
     /**
-     * Possible 'os.name'
+     Possible 'os.name'
      * AIX, Amiga OS, Android, Arch, Bada, BeOS, BlackBerry, CentOS, Chromium OS, Contiki,
      * Fedora, Firefox OS, FreeBSD, Debian, DragonFly, Gentoo, GNU, Haiku, Hurd, iOS,
      * Joli, Linpus, Linux, Mac OS, Mageia, Mandriva, MeeGo, Minix, Mint, Morph OS, NetBSD,
      * Nintendo, OpenBSD, OpenVMS, OS/2, Palm, PCLinuxOS, Plan9, Playstation, QNX, RedHat,
      * RIM Tablet OS, RISC OS, Sailfish, Series40, Slackware, Solaris, SUSE, Symbian, Tizen,
      * Ubuntu, UNIX, VectorLinux, WebOS, Windows [Phone/Mobile], Zenwalk
-     */
+     /
     name: string | undefined;
     /**
      * Determined dynamically
-     */
+     /
     version: string | undefined;
 }
 
 interface ICPU {
     /**
-     * Possible architecture:
-     *  68k, amd64, arm, arm64, avr, ia32, ia64, irix, irix64, mips, mips64, pa-risc,
-     *  ppc, sparc, sparc64
-     */
+     Possible architecture:
+     * 68k, amd64, arm, arm64, avr, ia32, ia64, irix, irix64, mips, mips64, pa-risc,
+     * ppc, sparc, sparc64
+     /
     architecture: string | undefined;
 }
 
@@ -93,7 +91,7 @@ interface UaInfo {
     device: IDevice;
     engine: IEngine;
     os: IOS;
-    cpu: ICPU;
+    CPU: ICPU;
 }
 export type Type = "string" | "number" | "boolean" | "undefined" | 'json'
 
@@ -112,67 +110,67 @@ type Functions = 'audio' | 'canvas' | 'webgl' | 'fonts' | 'webgpu' |
     'screen' | 'location' | 'navigator' | 'feature' | 'media' | 'worker' | 'iframe'
 
 export interface Browser {
-    //环境名称
+    //Environment Name
     name?: string
-    //随机种子
+    //Random seed
     seed?: number
-    //安全模式 开启后不修改浏览版本和系统版本
+    //Safe mode will not modify browser and system versions.
     safeMode?: boolean
-    //开启后进微调webgl版本信息
+    //Enable fine-tuning of WebGL version information
     webglSafeMode?: boolean
-    //开启后阻止worker通信
+    //Enabled to prevent worker communication
     disableWorker?: boolean
-    //阻止无限调试(需搭配driverCheater)
+    //Prevent infinite debugging (requires driverCheater)
     antiDebugger?: boolean
-    //函数跟踪
+    //Function tracing
     functionTrace?: boolean
-    //环境拟人
+    // Personification of the environment
     humanLike?: boolean
     //ua
     userAgent?: string,
-    //ua解析结果
+    //UA parsing result
     uaInfo?: UaInfo,
-    //自定义原型链的返回值
+    // Return value of custom prototype chain
     customProtos: Proto[],
-    //自定义对象的值
+    //Values ​​of custom objects
     customVars: Var[],
-    //伪装的webrtc ip地址
+    //Fake WebRTC IP address
     webrtc?: string
-    //地理坐标
+    //Geographic coordinates
     location?: {
         lng?: number,
         lat?: number,
     },
-    //时区code
+    //Timezone code
     timezone?: string
-    //语言code
+    //language code
     language?: string
-    //硬件/环境指纹干扰因子
+    //Hardware/Environmental fingerprint interference factors
     factors: {
-        //音频
+        //Audio
         audio: number
-        //画布
+        //canvas
         canvas: number
-        //字体
+        //Font
         fonts: number
-        //插件
+        //Plugin
         plugins: number
         //webgl
-        webgl: number
-        //webgpu
+        WebGL: Number
+        //WebGPU
         webgpu: number
-        //声音
+        //sound
         voice: number
-        //矩形
+        //rectangle
         clientRect: number
     },
-    //webgl 参数信息
+    //WebGL parameter information
     webglInfo: {
         [k: string]: any
     },
-    //控制cheater功能启用
+    //Control cheater function to enable
     enables: { [k in Functions]: boolean },
-    //屏幕信息 -1表示忽略
+    //Screen information -1 indicates ignore
     screen: {
         noise: number | -1,
         pixelDepth: number | -1,
@@ -180,9 +178,9 @@ export interface Browser {
         maxTouchPoints: number | -1,
         dpr: number | -1
     },
-    //内存大小
+    //Memory size
     memoryCapacity?: 0.25 | 0.5 | 1 | 2 | 4 | 8,
-    //处理器内核数量
+    //Number of processor cores
     processors?: 1 | 2 | 4 | 8 | 16 | 32 | 64,
   
 }
