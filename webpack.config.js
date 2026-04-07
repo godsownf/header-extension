e99ab5f01c827db3be2701eacb0b2340bb161ea2
magit notif <231088448+godsownf@users.noreply.github.com>
2026-04-07 09:16:54 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
 let out = "/public/" + process.env.npm_lifecycle_event.split(" ")[1]
+
 module.exports = {
-optimization: {
-minimize: true
-},
-entry: {
-//environment parameter generation code
-generator: "./src/generator.js",
-//fingerprint modification logic code
-window: "./src/window.js",
-//test code - randomly generate parameters to modify the fingerprint environment
-randomTest: "./src/randomTest.js",
-},
-output: {
-filename: '[name].js',
-path: __dirname + out,
-},
-plugins: [],
-module: {}
+    optimization: {
+        minimize: true
+    },
+    entry: {
+        //environment parameter generation code
+        generator: "./src/generator.js",
+        //fingerprint modification logic code
+        window: "./src/window.js",
+        //test code - randomly generate parameters to modify the fingerprint environment
+        randomTest: "./src/randomTest.js",
+    },
+    output: {
+        filename: '[name].js',
+        path: __dirname + out,
+    },
+    plugins: [],
+    module: {}
+
 }
