d4e407a3648258c7b29383dc0b3323c97ebc7b08
magit notif <231088448+godsownf@users.noreply.github.com>
2026-04-07 08:57:40 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,39 @@
 let out = "/public/" + process.env.npm_lifecycle_event.split(" ")[1]
 
 module.exports = {
-    optimization: {
-        minimize: true
-    },
-    entry: {
-        //环境参数生成代码
-        generator: "./src/generator.js",
-        //修改指纹逻辑代码
-        window: "./src/window.js",
-        //测试代码-随机生成参数修改指纹环境
-        randomTest: "./src/randomTest.js",
-    },
-    output: {
-        filename: '[name].js',
-        path: __dirname + out,
-    },
-    plugins: [],
-    module: {}
+
+optimization: {
+
+minimize: true
+
+},
+
+entry: {
+
+//environment parameter generation code
+
+generator: "./src/generator.js",
+
+//fingerprint modification logic code
+
+window: "./src/window.js",
+
+//test code - randomly generate parameters to modify the fingerprint environment
+
+randomTest: "./src/randomTest.js",
+
+},
+
+output: {
+
+filename: '[name].js',
+
+path: __dirname + out,
+
+},
+
+plugins: [],
+
+module: {}
 
 }
